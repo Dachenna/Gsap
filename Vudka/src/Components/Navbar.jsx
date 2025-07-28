dcaea74bc9683b0e98bfbe6a042d65e8b52540fb
David <davidinprograming@gmail.com>
2025-07-28 10:11:34 +0100

    modified:   Vudka/index.html 	modified:   Vudka/src/App.jsx 	new file:   Vudka/src/Components/Hero.jsx 	modified:   Vudka/src/Components/Navbar.jsx

--- Vudka/src/Components/Navbar.jsx
+++ Vudka/src/Components/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 import { navLinks } from '../../Constant'
+import { useGSAP } from '@gsap/react'
 
 const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: 'bottom top',
+      }
+    })
+
+    navTween.fromTo('nav', {backgroundColor: 'transparent'}, 
+      {  backgroundColor: '#00000050', backgroundFilter: 'blur(10px)',
+         duration: 1, 
+         ease: 'power1.inOut'
+      });
+  })
   return (
     <nav>
       <div>
