44862bd78cf8c33390a83f400a0cdda2bd1c4dab
David <davidinprograming@gmail.com>
2025-08-14 18:12:13 +0100

    Added ffmpeg for smooth scroll transition

--- Vudka/src/Components/Hero.jsx
+++ Vudka/src/Components/Hero.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { gsap } from 'gsap'
 import { SplitText } from 'gsap/SplitText'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { useGSAP } from '@gsap/react'
+import { useMediaQuery } from 'react-responsive'
 
 
 function Hero () {
+  const videoRef = useRef();
+
+    const isMobile =  useMediaQuery({ maxWidth: 767 });
+
   useGSAP(() => {
     const heroSplit = new SplitText('.title', { type: 'chars, words'});
     const paragraphSplit = new SplitText('.subtitle', { type: 'lines'});
@@ -38,6 +43,25 @@ function Hero () {
     })
       .to('.right-leaf', { y: 200 }, 0)
       .to('.left-leaf', { y: -200 }, 0)
+
+     const startValue = isMobile ? 'top 50%' : 'center 60%';
+     const endValue = isMobile? '120% top' : 'bottom top' 
+
+     //Video animation timeline
+     const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'video',
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      }
+     })
+     videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      })
+     }
 
   }, [])
 
@@ -75,6 +99,17 @@ function Hero () {
               </div>
             </div>
         </section>
+
+        <div className='video absolute inset-0'>
+          <video 
+            ref={videoRef}
+            src="/videos/input.mp4"
+            muted
+            playsInline
+            preload='auto'
+            
+          />
+        </div>
     </>
   )
 }
