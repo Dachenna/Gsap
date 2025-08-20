f0d440475069c6590093a492c25ad8028327f868
David <davidinprograming@gmail.com>
2025-08-20 14:33:52 +0100

    Implement Hero Changes

--- Vudka/src/Components/Cocktails.jsx
+++ Vudka/src/Components/Cocktails.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 function Cocktails() {
   return (
-    <section id='cocktails'>
+    <section id='cocktails' className='noisy'>
+        <img src="/images/cocktail-left-leaf.png" alt="left-leaf" id='c-left-leaf' />
+        <img src="/images/cocktail-right-leaf.png" alt="right-leaf" id='c-right-leaf' />
 
+        <div className="list">
+            <div className="popular">
+                <h2>Most Populat Vudka</h2>
+
+                <ul>
+                    
+                </ul>
+            </div>
+        </div>
     </section>
   )
 }
